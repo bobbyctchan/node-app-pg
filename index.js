3c062c2626d646af22c336522c154064f98129d4
bobby chan <bobbyc@bobbys-MacBook-Pro-2.local>
2022-12-02 22:42:32 -0800

    added db

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index', { title: "My App" }))
+app.get('/db', (req,res)=> {
+    data = [
+        {name:'bob',age:41},
+        {name:'jane',age:44}
+    ]
+    res.render('db', {result: data})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
